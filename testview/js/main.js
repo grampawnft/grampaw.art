61d2bf07a05273dd12514a69a6582f4b3b438a00
Novrizal Syailendra <novrizalsyailendra@gmail.com>
2022-02-12 20:36:51 +0700

    newtest

--- testview/js/main.js
+++ testview/js/main.js
@@ -76,27 +76,27 @@ $('.photobanner').each(function(index, value){
 });
 
 
-// $(document).bind("contextmenu",function(e){
-//     return false;
-// });
+$(document).bind("contextmenu",function(e){
+    return false;
+});
 
-// $(document).keydown(function(e){
-//   	if(event.keyCode == 123) {
-// 	    return false;
-// 	}
-// 	if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-// 	    return false;
-// 	}
-// 	if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-// 	    return false;
-// 	}
-// 	if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-// 	    return false;
-// 	}
-// 	if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-// 	    return false;
-// 	}
-// });
+$(document).keydown(function(e){
+  	if(event.keyCode == 123) {
+	    return false;
+	}
+	if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+	    return false;
+	}
+	if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+	    return false;
+	}
+	if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+	    return false;
+	}
+	if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+	    return false;
+	}
+});
 
 
 $(document).ready(function() {
