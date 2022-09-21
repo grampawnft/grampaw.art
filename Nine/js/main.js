a47bd7d644b5d66e1c86d90580ed74ff4c9a6204
Novrizal Syailendra <novrizalsyailendra@gmail.com>
2022-09-21 18:11:53 +0700

    testes

--- Nine/js/main.js
+++ Nine/js/main.js
@@ -49,10 +49,10 @@ barba.init({
 	views: [{
 	    namespace: 'home',
 	    afterEnter(data) {
-	    	if(!window.location.hash) {
-		        window.location = window.location + '#loaded';
-		        window.location.reload();
-		    }
+	    	// if(!window.location.hash) {
+		    //     window.location = window.location + '#loaded';
+		    //     window.location.reload();
+		    // }
 	    	var tl = gsap.timeline();
 
 	    	tl.to('.slide__caption', { duration: 1.5, translateY: 0, opacity: 1}, '-=1')
@@ -62,69 +62,7 @@ barba.init({
 	  }]
 })
 
-gsap.registerPlugin(ScrollTrigger);
 
-function batch(targets, vars) {
-  let varsCopy = {},
-      interval = vars.interval || 0.1,
-      proxyCallback = (type, callback) => {
-        let batch = [],
-            delay = gsap.delayedCall(interval, () => {callback(batch); batch.length = 0;}).pause();
-        return self => {
-          batch.length || delay.restart(true);
-          batch.push(self.trigger);
-          vars.batchMax && vars.batchMax <= batch.length && delay.progress(1);
-        };
-      },
-      p;
-  for (p in vars) {
-    varsCopy[p] = (~p.indexOf("Enter") || ~p.indexOf("Leave")) ? proxyCallback(p, vars[p]) : vars[p];
-  }
-  gsap.utils.toArray(targets).forEach(target => {
-    let config = {};
-    for (p in varsCopy) {
-      config[p] = varsCopy[p];
-    }
-    config.trigger = target;
-    ScrollTrigger.create(config);
-  });
-}
-
-batch(".news-item", {
-  interval: 0.1, 
-  batchMax: 3,   
-  onEnter: batch => gsap.to(batch, {autoAlpha: 1, y: 0, stagger: 0.3, overwrite: true, duration: .6})
-});
-
-gsap.to('.info-banner',{
-	scrollTrigger: {
-		trigger: '.info-banner'
-	},
-	duration: .6,
-	y: 15,
-	autoAlpha: 1,
-	ease: "back.out(3)"
-});
-
-gsap.to('.image-banner',{
-	scrollTrigger: {
-		trigger: '.image-banner',
-		start: 'center 80%',
-	},
-	duration: 2,
-	scale: 1,
-	opacity: 1,
-	ease: "back.out(2)"
-}, );
-
-gsap.to('.footer',{
-	scrollTrigger: {
-		trigger: '.footer',
-	},
-	duration: 1.5,
-	opacity: 1,
-	ease: "power4.out)"
-}, );
 
 
 // form js
