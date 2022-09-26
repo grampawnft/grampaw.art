9361713b962d4ad05c4c3b9f2e5c95d5f509677f
Novrizal Syailendra <novrizalsyailendra@gmail.com>
2022-09-26 11:08:44 +0700

    coba2 terus

--- Nine/js/plugins.js
+++ Nine/js/plugins.js
@@ -7682,6 +7682,73 @@ var _slideshow = require("./slideshow");
   slideshow.on('updateCurrent', function (position) {
     return navigation.updateCurrent(position);
   });
+
+  function pageTransition(){
+
+	var tl = gsap.timeline();
+
+	tl.to('.main-container', { duration: .5, opacity: 1})
+	tl.to('ul.transition li', { duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
+	tl.to('ul.transition li', { duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
+	tl.to('.main-container', { duration: .5, opacity: 0, delay: .1})
+}
+
+function contentAnimation(){
+
+	var tl = gsap.timeline();
+
+	tl.from('header', { duration: 1.5, opacity: 0})
+
+}
+
+function delay(n){
+	n = n || 2000;
+	return new Promise(done => {
+		setTimeout(() => {
+			done();
+		}, n);
+	});
+}
+
+barba.init({
+
+	sync: true,
+	transitions: [{
+		async leave(data){
+
+			const done = this.async();
+			pageTransition();
+			await delay(1500);
+			done();
+
+		},
+
+		async enter(data){
+			contentAnimation();
+		},
+		async once(data){
+			contentAnimation()
+		}
+	}],
+	views: [{
+	    namespace: 'home',
+	    afterEnter(data) {
+	    	// if(!window.location.hash) {
+		    //     window.location = window.location + '#loaded';
+		    //     window.location.reload();
+		    // }
+
+		    slideshow.next();
+
+	    	var tl = gsap.timeline();
+
+	    	tl.to('.slide__caption', { duration: 1.5, translateY: 0, opacity: 1}, '-=1')
+			tl.to('.slide__img-wrap', { duration: 1, clipPath: 'circle(55% at 70% 50%)', opacity: 1}, '-=1.2');
+
+	    }
+	  }]
+})
+
 });
 },{"../utils":"js/utils.js","../navigation":"js/navigation.js","./slideshow":"js/demo1/slideshow.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
