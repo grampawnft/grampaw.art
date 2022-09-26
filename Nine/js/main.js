05914fb189033db593acd73c08f4a1a8b2b3547b
Novrizal Syailendra <novrizalsyailendra@gmail.com>
2022-09-26 11:53:32 +0700

    barba ngawur

--- Nine/js/main.js
+++ Nine/js/main.js
@@ -1,5 +1,66 @@
 
+function pageTransition(){
 
+	var tl = gsap.timeline();
+
+	tl.to('.main-container', { duration: .5, opacity: 1})
+	tl.to('ul.transition li', { duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
+	tl.to('ul.transition li', { duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
+	tl.to('.main-container', { duration: .5, opacity: 0, delay: .1})
+}
+
+function contentAnimation(){
+
+	var tl = gsap.timeline();
+
+	tl.from('header', { duration: 1.5, opacity: 0})
+
+}
+
+function delay(n){
+	n = n || 2000;
+	return new Promise(done => {
+		setTimeout(() => {
+			done();
+		}, n);
+	});
+}
+
+barba.init({
+
+	sync: true,
+	transitions: [{
+		async leave(data){
+
+			const done = this.async();
+			pageTransition();
+			await delay(1500);
+			done();
+
+		},
+
+		async enter(data){
+			contentAnimation();
+		},
+		async once(data){
+			contentAnimation()
+		}
+	}],
+	views: [{
+	    namespace: 'home',
+	    afterEnter(data) {
+	    	// if(!window.location.hash) {
+		    //     window.location = window.location + '#loaded';
+		    //     window.location.reload();
+		    // }
+	    	var tl = gsap.timeline();
+
+	    	tl.to('.slide__caption', { duration: 1.5, translateY: 0, opacity: 1}, '-=1')
+			tl.to('.slide__img-wrap', { duration: 1, clipPath: 'circle(55% at 70% 50%)', opacity: 1}, '-=1.2');
+
+	    }
+	  }]
+})
 
 gsap.registerPlugin(ScrollTrigger);
 
--- Nine/js/plugins.js
+++ Nine/js/plugins.js
@@ -7688,68 +7688,6 @@ var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
 
-function pageTransition(){
-
-	var tl = gsap.timeline();
-
-	tl.to('.main-container', { duration: .5, opacity: 1})
-	tl.to('ul.transition li', { duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
-	tl.to('ul.transition li', { duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
-	tl.to('.main-container', { duration: .5, opacity: 0, delay: .1})
-}
-
-function contentAnimation(){
-
-	var tl = gsap.timeline();
-
-	tl.from('header', { duration: 1.5, opacity: 0})
-
-}
-
-function delay(n){
-	n = n || 2000;
-	return new Promise(done => {
-		setTimeout(() => {
-			done();
-		}, n);
-	});
-}
-
-barba.init({
-
-	sync: true,
-	transitions: [{
-		async leave(data){
-
-			const done = this.async();
-			pageTransition();
-			await delay(1500);
-			done();
-
-		},
-
-		async enter(data){
-			contentAnimation();
-		},
-		async once(data){
-			contentAnimation()
-		}
-	}],
-	views: [{
-	    namespace: 'home',
-	    afterEnter(data) {
-	    	
-	    	var tl = gsap.timeline();
-
-	    	tl.to('.slide__caption', { duration: 1.5, translateY: 0, opacity: 1}, '-=1')
-			tl.to('.slide__img-wrap', { duration: 1, clipPath: 'circle(55% at 70% 50%)', opacity: 1}, '-=1.2');
-
-			slideshow.init();
-
-	    }
-	  }]
-})
-
 function Module(moduleName) {
   OldModule.call(this, moduleName);
   this.hot = {
