2ed06b79541e11f4bb2541d5db7a3eb1254ff3f9
Novrizal Syailendra <novrizalsyailendra@gmail.com>
2022-09-29 11:07:22 +0700

    plugins barba syit

--- showreel/rekagunatek/js/plugins.js
+++ showreel/rekagunatek/js/plugins.js
@@ -4445,8 +4445,109 @@ barba.init({
 	}],
 	views: [{
 	    namespace: 'home',
+	    beforeEnter(data){
+	    	var script = document.getElementById("hometransition");
+	    	$(script).remove();
+	    },
 	    afterEnter(data) {
 	    	preloadImages('.slide__img img').then(function () {
+	    		var offset;
+			    function polylineToPath() {
+
+			        var polyline = document.querySelectorAll('polyline, polygon');
+
+					[].forEach.call(polyline, function(poly) {
+						var svgNS = poly.ownerSVGElement.namespaceURI;
+						var path = document.createElementNS(svgNS, 'path');
+						var points = poly.getAttribute('points').split(/\s+|,/);
+							var x0=points.shift(), y0=points.shift();
+						var pathdata = 'M'+x0+','+y0+'L'+points.join(' ');
+							if (poly.tagName=='polygon') pathdata+='z';
+						path.setAttribute('class', 'p');
+						path.setAttribute('d',pathdata);
+						var len = path.getTotalLength();
+						path.style.strokeDashoffset = len;
+						path.style.strokeDasharray = len + ',' + len;
+						poly.parentNode.replaceChild(path, poly);
+						animate();
+						animatePath();
+						setInterval(function () {
+					    	animate();
+					    	animatePath();
+			  			}, 8000);
+						
+					});
+			    }
+
+			    function animate() {
+			        var paths = document.querySelectorAll('.p');
+
+					[].forEach.call(paths, function(path) {
+					  var length = path.getTotalLength();
+					  // Clear any previous transition
+					  path.style.transition = path.style.WebkitTransition =
+					    'none';
+					  // Set up the starting positions
+					  path.style.strokeDasharray = length + ' ' + length;
+					  path.style.strokeDashoffset = length;
+					  // Trigger a layout so styles are calculated & the browser
+					  // picks up the starting position before animating
+					  path.getBoundingClientRect();
+					  // Define our transition
+					  path.style.transition = path.style.WebkitTransition =
+					    'stroke-dashoffset 5s';
+					  // Go!
+					  path.style.strokeDashoffset = '0';
+					})
+			    }
+
+			    function animatePath(){
+			    	var paths = document.querySelectorAll('.st0, .st1');
+
+					[].forEach.call(paths, function(path) {
+					  var length = path.getTotalLength();
+					  // Clear any previous transition
+					  path.style.transition = path.style.WebkitTransition =
+					    'none';
+					  // Set up the starting positions
+					  path.style.strokeDasharray = length + ' ' + length;
+					  path.style.strokeDashoffset = length;
+					  // Trigger a layout so styles are calculated & the browser
+					  // picks up the starting position before animating
+					  path.getBoundingClientRect();
+					  // Define our transition
+					  path.style.transition = path.style.WebkitTransition =
+					    'stroke-dashoffset 2s';
+					  // Go!
+					  path.style.strokeDashoffset = '0';
+					})
+			    }
+
+			    setTimeout(function() {
+			    	$('.hero-slider-wrapper').on('init', function(event, slick){
+					    $(".hero-slider-wrapper").find('.slick-current').addClass("active");
+					});
+					$('.hero-slider-wrapper').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+					    $(".hero-slider-wrapper").find('.hero-slider-item').removeClass("active");
+				    	$(".hero-slider-wrapper").find('.hero-slider-item').eq(nextSlide).addClass("active");
+					});
+					$('.hero-slider-wrapper').slick({
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						speed: 100,
+						arrows: false,
+						fade: true,
+						dots: true,
+						infinite: true,
+						autoplay: true,
+						autoplaySpeed: 8000,
+						focusOnSelect: false,
+		  				pauseOnHover:false
+					});
+					 polylineToPath();
+					 $('.svg-anim, .svg-anim2').css('opacity','1');
+			    }, 500); 
+
 				// remove loader (loading class) 
 				// document.body.classList.remove('loading');
 				var slideshow = new _slideshow.Slideshow(document.querySelector('.slideshow'));
