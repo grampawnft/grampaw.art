f5e4710b837ad07dc4e6c4f332b4d053a503b121
Novrizal Syailendra <novrizalsyailendra@gmail.com>
2022-09-29 17:50:37 +0700

    rekamenu

--- showreel/rekagunatek/js/main.js
+++ showreel/rekagunatek/js/main.js
@@ -1,16 +1,25 @@
 
+
 $(document).on('click', '.js-hamburger',  function(e){
 	e.stopPropagation();
 	e.preventDefault();
 
-	var _parent = $(this).closest('header');
-	var _sibmobile = _parent.siblings('.mobile-menu-list');
-	var _sibBGoverlay = _parent.siblings('.bg-overlay');
+	var tl = gsap.timeline();
 
-	// _sibmobile.addClass('active');
-	// _sibBGoverlay.addClass('active');
-	// $('body').addClass('noscroll')
+	tl.to('.menu-wrapper', { duration: .8, scaleX: 1, ease: 'expo.out'});
+	tl.to('ul.menu-list li', { duration: .8, opacity: 1, x: 0, ease: 'expo.out', stagger: .2}, '-=.2');
+	tl.to('.company-info', { duration: .8, opacity: 1, x: 0, ease: 'expo.out'}, '-=.55');
 
-	$(this).toggleClass('active');
+});
+
+$(document).on('click', '.js-close-menu',  function(e){
+	e.stopPropagation();
+	e.preventDefault();
+
+	var tl = gsap.timeline();
+
+	tl.to('ul.menu-list li', { duration: .6, opacity: 0, x: 50, ease: 'expo.out', stagger: .1});
+	tl.to('.company-info', { duration: .8, opacity: 0, x: 50, ease: 'expo.out'}, '-=.4');
+	tl.to('.menu-wrapper', { duration: .8, scaleX: 0, ease: 'power4.out'}, '-=.5');
 
 });
