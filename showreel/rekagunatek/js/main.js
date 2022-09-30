1aae33f559e1511e56c4ee321857c7c3ee537d39
Novrizal Syailendra <novrizalsyailendra@gmail.com>
2022-09-30 09:31:30 +0700

    section3

--- showreel/rekagunatek/js/main.js
+++ showreel/rekagunatek/js/main.js
@@ -7,7 +7,7 @@ $(document).on('click', '.js-hamburger',  function(e){
 	var tl = gsap.timeline();
 
 	tl.to('.menu-wrapper', { duration: .8, scaleX: 1, ease: 'expo.out'});
-	tl.to('ul.menu-list li', { duration: .8, opacity: 1, x: 0, ease: 'expo.out', stagger: .2}, '-=.2');
+	tl.to('ul.menu-list li', { duration: .8, opacity: 1, x: 0, ease: 'expo.out', stagger: .2}, '-=.4');
 	tl.to('.company-info', { duration: .8, opacity: 1, x: 0, ease: 'expo.out'}, '-=.55');
 
 });
@@ -23,3 +23,43 @@ $(document).on('click', '.js-close-menu',  function(e){
 	tl.to('.menu-wrapper', { duration: .8, scaleX: 0, ease: 'power4.out'}, '-=.5');
 
 });
+
+let slideIndex = 0;
+showSlides();
+
+// Next-previous control
+function nextSlide() {
+  slideIndex++;
+  showSlides();
+  timer = _timer; // reset timer
+}
+
+function showSlides() {
+  let slides = document.querySelectorAll(".leader-slide-item");
+
+  if (slideIndex > slides.length - 1) slideIndex = 0;
+  if (slideIndex < 0) slideIndex = slides.length - 1;
+  
+  // hide all slides
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  
+  // show one slide base on index number
+  slides[slideIndex].style.display = "block";
+  
+}
+
+// autoplay slides --------
+let timer = 3; // sec
+const _timer = timer;
+
+// this function runs every 1 second
+setInterval(() => {
+  timer--;
+
+  if (timer < 1) {
+    nextSlide();
+    timer = _timer; // reset timer
+  }
+}, 1700); // 1sec
